595e60f11867b74d8181161ae3cf8fcfdb219877
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2022-03-31 09:03:44 +0200

    master

--- index.php
+++ index.php
@@ -1,18 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Code</title>
-  </head>
-  <body>
-    <h1>Hallo!</h1>
-    <p>Dies ist eine weitere Website!</p>
-    <p>Auf dieser Website zeige ich euch alle unwichtigen Sachen!</p>
-    <a href="test.php">Helllo</a>
-    <?php 
-    echo 3
-     ?>
-  </body>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h1>Hallo Welt</h1>
+
+    <p> Hallo, das ist ein Paragraph. </p>
+
+    <?php
+    $name = 'Anton';
+    echo '<p>Mein Name ist ' . $name . '</p>';
+
+
+    $zahl1 = 7;
+    $zahl2 = 8;
+
+    echo '<p>7 * 8 = ' . $zahl1 * $zahl2 . '</p>';
+    ?>
+
+
+
+</body>
+
 </html>
